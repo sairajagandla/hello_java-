348eb10091d843a72c6c5e40674521db26cf499a
sairajagandla <sairajagandla@gmail.com>
2022-02-12 15:06:57 +0000

    added multiple statements

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, baby good morning");
+    System.out.println("Hello, baby good morningSystem.out.println("Hello, baby good morning"); 
+		    System.out.println("Hello, baby good morning");System.out.println("Hello, baby good morning");System.out.println("Hello, baby good morning");System.out.println("Hello, baby good morning");System.out.println("Hello, baby good morning");System.out.println("Hello, baby good morning");");
   }
 }
